0a7bee0837ed008329c6fc67a857569a71387246
chrismbah <mbahchris46@gmail.com>
2025-03-19 12:29:09 +0100

    remove read message api

--- src/routes/message.js
+++ src/routes/message.js
@@ -4,12 +4,10 @@ const {
   sendMessage,
   getMessagesByChatId,
   deleteMessage,
-  markMessagesAsRead,
 } = require("../controllers/message");
 
 const router = express.Router();
 router.post("/", validateToken, sendMessage);
 router.get("/", validateToken, getMessagesByChatId);
 router.delete("/:messageId", validateToken, deleteMessage);
-router.patch("/read", validateToken, markMessagesAsRead);
 module.exports = router;
